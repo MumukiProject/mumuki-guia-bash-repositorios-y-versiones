29a6c1b6a7eeeb10e710a50d58adcefb76dc1e04
franco@mumuki.org <franco@mumuki.org>
2018-01-03 16:45:18 -0500

    Mumuki Export on 2018-01-03 16:45:18 -0500

---
+++ 00012_Commit tras commit/extra.sh
@@ -0,0 +1,8 @@
+mkdir autobiografia
+cd autobiografia
+git init .
+echo "siempre digo que el artista que se apodera de la idea de otro enturbia las aguas del manantial del espíritu" >> capitulo1.txt
+git add capitulo1.txt
+git commit -m "Primera versión"
+touch capitulo2.txt
+echo "Günther Frager." >> capitulo1.txt
