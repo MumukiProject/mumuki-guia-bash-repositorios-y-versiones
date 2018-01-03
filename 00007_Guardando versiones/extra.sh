70dfcf378152599b0595ad2e71143a75211560c1
franco@mumuki.org <franco@mumuki.org>
2018-01-03 14:36:58 -0500

    Mumuki Export on 2018-01-03 14:36:58 -0500

--- 00007_Guardando versiones/extra.sh
+++ 00007_Guardando versiones/extra.sh
@@ -1,5 +1,5 @@
 mkdir autobiografia
 cd autobiografia
 git init .
-cat "siempre digo que el artista que se apodera de la idea de otro enturbia las aguas del manantial del espíritu" >> capitulo1.
+cat "siempre digo que el artista que se apodera de la idea de otro enturbia las aguas del manantial del espíritu" >> capitulo1.txt
 cd ..
