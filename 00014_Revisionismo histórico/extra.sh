23887935fdba2a0d45c59e0494578140b8d297f4
agus@mumuki.org <agus@mumuki.org>
2018-04-19 15:04:10 -0400

    Mumuki Export on 2018-04-19 15:04:10 -0400

--- 00014_Revisionismo histórico/extra.sh
+++ 00014_Revisionismo histórico/extra.sh
@@ -3,7 +3,7 @@ cd misterio
 git init .
 touch capitulo1.txt
 git add capitulo1.txt
-git commit "Primer capítulo"
+git commit -m "Primer capítulo"
 touch capitulo2.txt
 git add capitulo2.txt
-git commit "Segundo capítulo"
+git commit -m "Segundo capítulo"
