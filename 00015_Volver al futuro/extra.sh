6c21cd7616ec70b6774e419faacd77f74394e3d3
agus@mumuki.org <agus@mumuki.org>
2018-04-19 15:07:09 -0400

    Mumuki Export on 2018-04-19 15:07:09 -0400

---
+++ 00015_Volver al futuro/extra.sh
@@ -0,0 +1,10 @@
+mkdir misterio
+cd misterio
+git init .
+touch capitulo1.txt
+git add capitulo1.txt
+git commit -m "Primer capítulo"
+touch misterioso.txt
+echo "¡Felicitaciones!" >> misterioso.txt
+git commit -m "Final de la guía 2"
+git checkout HEAD~1
